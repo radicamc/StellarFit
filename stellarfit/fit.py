258cddf31cb5caf9a22c2b4b6a74ee57cbf3733a
radicamc <radicamc@uchicago.edu>
2024-11-13 09:29:58 -0600

    Update fit.py

--- stellarfit/fit.py
+++ stellarfit/fit.py
@@ -14,6 +14,7 @@ from dynesty import NestedSampler
 from dynesty.utils import resample_equal
 import emcee
 import h5py
+from multiprocessing import Pool
 import numpy as np
 import os
 from pathlib import Path
@@ -62,7 +63,7 @@ class Dataset:
         self.mcmc_sampler = None
         self.nested_sampler = None
 
-    def fit(self, output_file, sampler='MCMC', mcmc_start=None,
+    def fit(self, output_file, sampler='MCMC', mcmc_start=None, mcmc_ncores=1,
             mcmc_steps=10000, continue_mcmc=False, dynesty_args=None):
         """Run a spectrum fit.
 
@@ -74,6 +75,8 @@ class Dataset:
             Sampling to use, either 'MCMC' or 'Nested Sampling'.
         mcmc_start : ndarray(float)
             Starting positions for MCMC sampling. MCMC only.
+        mcmc_ncores : int
+            Number of cores for multiprocessing. MCMC only.
         mcmc_steps : int
             Number of steps to take for MCMC sampling. MCMC only.
         continue_mcmc : bool
@@ -127,7 +130,8 @@ class Dataset:
                                      silent=self.silent, mcmc_steps=mcmc_steps,
                                      log_probability_args=log_prob_args,
                                      output_file=output_file,
-                                     continue_run=continue_mcmc)
+                                     continue_run=continue_mcmc,
+                                     ncores=mcmc_ncores)
             self.mcmc_sampler = mcmc_sampler
 
         # For Nested Sampling with dynesty.
@@ -354,7 +358,8 @@ def fit_dynesty(prior_transform, log_like, ndim, output_file,
 
 
 def fit_emcee(log_prob, output_file, initial_pos=None, continue_run=False,
-              silent=False, mcmc_steps=10000, log_probability_args=None):
+              silent=False, mcmc_steps=10000, log_probability_args=None,
+              ncores=1):
     """Run a light curve fit via MCMC using the emcee sampler.
 
     Parameters
@@ -428,9 +433,11 @@ def fit_emcee(log_prob, output_file, initial_pos=None, continue_run=False,
         fancyprint('{} steps already completed.'.format(backend.iteration))
 
     # Do the sampling.
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, backend=backend,
-                                    args=log_probability_args)
-    output = sampler.run_mcmc(initial_pos, mcmc_steps, progress=not silent)
+    with Pool(processes=ncores) as pool:
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob,
+                                        backend=backend, pool=pool,
+                                        args=log_probability_args)
+        output = sampler.run_mcmc(initial_pos, mcmc_steps, progress=not silent)
 
     return sampler
 
